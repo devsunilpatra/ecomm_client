a543b4b69830508dd9e8fc8b0397f5cf1f29a3b3
Sunil <devsunilpatra@gmail.com>
2026-04-07 15:19:49 +0530

    feat: implement fontfamily

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p className="font-medium text-sm md:text-base">OUR BESTSELLER</p>
           </div>
 
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
 
