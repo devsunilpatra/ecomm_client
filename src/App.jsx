6e73ae67a7c470805612347039c8dfa3f849b84e
Sunil <devsunilpatra@gmail.com>
2026-04-12 10:20:09 +0530

    feat: footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 import PageSkeleton from "./components/ui/loadingSkeleton/PageSkeleton";
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar/Navbar";
+import Footer from "./components/layout/Footer";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -14,36 +15,25 @@ const Cart = lazy(() => import("./pages/Cart"));
 const PlaceOrder = lazy(() => import("./pages/PlaceOrder"));
 const Orders = lazy(() => import("./pages/Orders"));
 
-
-
 function App() {
   return (
-    
     <Container>
-    <Navbar/>
-        <Suspense fallback={<PageSkeleton />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/collection" element={<Collection />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/product/:productId" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/order" element={<Orders />} />
-          </Routes>
-        </Suspense>
+      <Navbar />
+      <Suspense fallback={<PageSkeleton />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/order" element={<Orders />} />
+        </Routes>
+      </Suspense>
+      <Footer />
     </Container>
-   
   );
 }
 export default App;
-
-
-
-
-
-
-
-
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,76 @@
-
-
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-8 md:my-10 md:mt-40 text-sm">
+        <div>
+          <Link to="/">
+            <img src={assets?.logo} alt="logo" className="mb-5 w-32" />
+          </Link>
 
-export default Footer
+          <p className="w-full md:w-2/3 text-gray-600 ">
+            Discover the latest collection featuring modern designs, premium
+            quality, and timeless style for every occasion. Please arrange me
+            the paragraph here
+          </p>
+        </div>
+
+        {/* Company Links */}
+        <nav aria-label="Company">
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-sm text-gray-500">
+            <li>
+              <Link to="/" className="hover:text-black transition">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:text-black transition">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/delivery" className="hover:text-black transition">
+                Delivery
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/privacy-policy"
+                className="hover:text-black transition"
+              >
+                Privacy Policy
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Contact Section */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-3 text-gray-600">
+            <li className="hover:text-black transition">
+              <Link href="tel:+918660925010">+91-8660925010</Link>
+            </li>
+            <li className="hover:text-black transition">
+              <Link href="mailto:itzsunilpatra@gmail.com">
+                itzsunilpatra@gmail.com
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Bottom Bar */}
+      <div>
+        <hr className="border-t border-gray-400" />
+        <p className="py-5 text-sm text-center text-gray-500">
+          © {new Date().getFullYear()} Leecart. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
